91c7e8295108b48f6d2f444546f4518f037b7a43
Durgavino <mailtodurga93@gmail.com>
2022-12-22 17:25:23 -0400

    added search buttons

--- script.js
+++ script.js
@@ -5,28 +5,36 @@
 
 var search = document.getElementById('search');
 
-
-
 function getdata() {
     search.addEventListener("click", function () {
 
         var country = document.getElementById("country").value;
-
         var searchresult = document.getElementById("searchresult");
+        var countryarray = localStorage.getItem("key");
 
-   
-    var countryarray=localStorage.getItem("key");
-    if(!countryarray){
-        countryarray=[country];
-    }
-    else{
-        countryarray=JSON.parse(countryarray);
-        countryarray.push(country);
-    }
+        if (!countryarray) {
+            countryarray = [country];
+        }
+        else {
+            countryarray = JSON.parse(countryarray);
+            countryarray.push(country);
+        }
 
-    localStorage.setItem("key",JSON.stringify(countryarray));
+        localStorage.setItem("key", JSON.stringify(countryarray));
 
-    searchresult.innerHTML=(localStorage.getItem("key"));
+        for (let i = 0; i < countryarray.length; i++) {
+            //new item
+            var x = document.createElement("button");
+
+            x.textContent = countryarray[i];
+            //  console.log(x);
+            //  console.log(countryarray[i]);
+            searchresult.appendChild(x);
+
+
+            x.style.color = "black";
+            x.style.backgroundColor = "green";
+        }
 
         const url = `http://api.openweathermap.org/data/2.5/forecast?q=${country}&appid=826c4a5299f1929f7d07c483556cc38d&cnt=50&units=imperial&lang=en`;
 
@@ -49,8 +57,6 @@ function getdata() {
                 document.getElementById("humidity1").innerHTML = "Humidity:" + humidity1 + "%";
                 document.getElementById("temp1").innerHTML = "Temp:" + temp1 + " °F";
 
-
-
                 date2 = (weatherdata[8].dt_txt);
                 wind2 = (weatherdata[8].wind.speed);
                 humidity2 = (weatherdata[8].main.humidity);
@@ -69,19 +75,14 @@ function getdata() {
                 document.getElementById("humidity3").innerHTML = "Humidity:" + humidity3 + "%";
                 document.getElementById("temp3").innerHTML = "Temp:" + temp3 + " °F";
 
-
-
                 date4 = (weatherdata[24].dt_txt);
                 wind4 = (weatherdata[24].wind.speed);
                 humidity4 = (weatherdata[24].main.humidity);
                 temp4 = (weatherdata[24].main.temp);
                 document.getElementById("date4").innerHTML = date4;
-
                 document.getElementById("wind4").innerHTML = "Wind:" + wind2 + " MPS";
                 document.getElementById("humidity4").innerHTML = "Humidity:" + humidity4 + "%";
                 document.getElementById("temp4").innerHTML = "Temp:" + temp4 + " °F";
-
-
 
                 date5 = (weatherdata[27].dt_txt);
                 wind5 = (weatherdata[27].wind.speed);
@@ -92,21 +93,13 @@ function getdata() {
                 document.getElementById("humidity5").innerHTML = "Humidity:" + humidity5 + "%";
                 document.getElementById("temp5").innerHTML = "Temp:" + temp5 + " °F";
 
-
-
-
-
             })
             .catch(function (err) {
                 console.log(err);
 
-
-
             }
 
-
             );
-
 
     }
     );
