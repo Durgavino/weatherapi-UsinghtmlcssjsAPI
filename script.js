d00dcbad41ecdbb1e1a748295cce66904a0e1179
Durgavino <mailtodurga93@gmail.com>
2022-12-22 23:46:01 -0400

    localweather data

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ fetch(url)
                 //   renderItems(city, data);
                 console.log(data);
                 var weatherdata = data.list;
-                console.log(weatherdata);
+                //console.log(weatherdata);
                
                 date1 = (weatherdata[0].dt_txt);
                 wind1 = (weatherdata[0].wind.speed);
@@ -143,6 +143,43 @@ fetch(url)
                 document.getElementById("humidity1").innerHTML = "Humidity:" + humidity1 + "%";
                 document.getElementById("temp1").innerHTML = "Temp:" + temp1 + " °F";
 
+                date2 = (weatherdata[8].dt_txt);
+                wind2 = (weatherdata[8].wind.speed);
+                humidity2 = (weatherdata[8].main.humidity);
+                temp2 = (weatherdata[8].main.temp);
+                document.getElementById("date2").innerHTML = date2;
+                document.getElementById("wind2").innerHTML = "Wind:" + wind2 + " MPS";
+                document.getElementById("humidity2").innerHTML = "Humidity:" + humidity2 + "%";
+                document.getElementById("temp2").innerHTML = "Temp:" + temp2 + " °F";
+
+                date3 = (weatherdata[16].dt_txt);
+                wind3 = (weatherdata[16].wind.speed);
+                humidity3 = (weatherdata[16].main.humidity);
+                temp3 = (weatherdata[16].main.temp);
+                document.getElementById("date3").innerHTML = date3;
+                document.getElementById("wind3").innerHTML = "Wind:" + wind2 + " MPS";
+                document.getElementById("humidity3").innerHTML = "Humidity:" + humidity3 + "%";
+                document.getElementById("temp3").innerHTML = "Temp:" + temp3 + " °F";
+
+                date4 = (weatherdata[24].dt_txt);
+                wind4 = (weatherdata[24].wind.speed);
+                humidity4 = (weatherdata[24].main.humidity);
+                temp4 = (weatherdata[24].main.temp);
+                document.getElementById("date4").innerHTML = date4;
+                document.getElementById("wind4").innerHTML = "Wind:" + wind2 + " MPS";
+                document.getElementById("humidity4").innerHTML = "Humidity:" + humidity4 + "%";
+                document.getElementById("temp4").innerHTML = "Temp:" + temp4 + " °F";
+
+                date5 = (weatherdata[27].dt_txt);
+                wind5 = (weatherdata[27].wind.speed);
+                humidity5 = (weatherdata[27].main.humidity);
+                temp5 = (weatherdata[27].main.temp);
+                document.getElementById("date5").innerHTML = date5;
+                document.getElementById("wind5").innerHTML = "Wind:" + wind2 + " MPS";
+                document.getElementById("humidity5").innerHTML = "Humidity:" + humidity5 + "%";
+                document.getElementById("temp5").innerHTML = "Temp:" + temp5 + " °F";
+
+              
             })
 
             .catch(function (err) {
